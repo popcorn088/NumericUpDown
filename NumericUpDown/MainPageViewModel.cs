950450f27e5520fb4068e7a9dc64a770fe4cef40
popcorn <popcorn088@gmail.com>
2024-03-29 23:56:19 +0900

    wip

---
+++ NumericUpDown/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using NumericUpDown.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericUpDown
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private NumericUpDownViewModel _numericUpDownViewModel = new NumericUpDownViewModel();
+        public NumericUpDownViewModel NumericUpDownViewModel
+        {
+            get
+            {
+                return _numericUpDownViewModel;
+            }
+            set
+            {
+                _numericUpDownViewModel = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumericUpDownViewModel)));
+            }
+        }
+    }
+}
---
+++ NumericUpDown/UserControls/NumericUpDownViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericUpDown.UserControls
+{
+    public class NumericUpDownViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _value;
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+    }
+}
